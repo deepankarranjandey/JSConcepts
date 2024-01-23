d6600048f65811c410d7bf5f5e470dd28ca5a498
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-23 19:36:12 +0530

    Check Prime Number

---
+++ src/Ques8.js
@@ -0,0 +1,14 @@
+
+//Check Prime Number
+
+function isPrime(num)
+{
+    for(let i=2; i<=num; i++)
+    {
+        if(num %2 ===0)
+        return false;
+    return num >1;
+    }
+}
+
+console.log(isPrime(7));
